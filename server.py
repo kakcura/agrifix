4cf7991486912f7ff6ba6aa17835a561b7fa22a4
Saif Kurdi-Teylouni <teylouniseif@gmail.com>
2019-10-20 10:34:16 -0400

    save imgs on server

--- server.py
+++ server.py
@@ -3,7 +3,12 @@
 import json
 import requests
 import logging
-import operator 
+import operator
+import requests
+from requests.auth import HTTPBasicAuth
+import os
+import glob, os
+
 
 from logging.handlers import RotatingFileHandler
 
@@ -14,14 +19,14 @@ usr = 'agrifix'
 tkn = 'a_-_-**_-_-9'
 
 @app.route("/", methods=['GET', 'POST'])
-def root(): 
+def root():
 	return app.send_static_file('index.html')
 
 @app.route('/coordinates', methods=['POST'])
 def listen():
 	try:
 		longitude = request.form.get("longitude")
-		latitude = request.form.get("latitude")	
+		latitude = request.form.get("latitude")
 	except Exception:
 		return jsonify({"response" : "Bad request!"}), 400
 
@@ -30,6 +35,11 @@ def listen():
 def getRadasat1Images(longitude, latitude):
 	radarsat1_api_url = "https://data.eodms-sgdot.nrcan-rncan.gc.ca/api/dhus/v1/products/Radarsat1/search?q=footprint:Intersects((" + latitude + "," + longitude + "))"
 	response = requests.get(radarsat1_api_url, auth=(usr, tkn)).json()
+	localdir=os.path.dirname(os.path.abspath(__file__))
+	test = localdir+"/static/images/radarsat1/original/*"
+	r = glob.glob(test)
+	for i in r:
+   		os.remove(i)
 
 	response_array = []
 	for entry in range(len(response['entry'])):
@@ -37,7 +47,12 @@ def getRadasat1Images(longitude, latitude):
 		linklist=response["entry"][entry]['link']
 		for el in range(len(linklist)):
 			if linklist[el].get('rel')=='alternative':
-				image_pair = {"date" : image_date, "image_url" : linklist[el].get('href')}
+				img=requests.get(linklist[el].get('href'), auth=HTTPBasicAuth("Saif Kurdi-Teylouni", "Workfreedom1!"), verify=False)
+				title=img.url.split("FeatureID=")[1].split("&")[0]+".png"
+				image_pair = {"date" : image_date, "image_url" : title}
+				if img.status_code == 200:
+				    with open(localdir+"/static/images/radarsat1/original/"+title, 'wb') as f:
+				        f.write(img.content)
 		response_array.append(image_pair)
 
 	# Sort the images by date.
@@ -50,4 +65,3 @@ if __name__ == '__main__':
 	handler.setLevel(logging.INFO)
 	app.logger.addHandler(handler)
 	app.run(host="127.0.0.1",port=5000)
-
